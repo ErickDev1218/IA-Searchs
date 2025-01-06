7881bce3dd2be9784bfbf39882a03e49581c3a49
ErickDev1218 <erickpdm879@gmail.com>
2025-01-06 14:11:11 -0300

    .

--- Node.py
+++ Node.py
@@ -2,3 +2,7 @@ class Node:
     def __init__(self, xValue : int, yValue : int):
         self.x = xValue if xValue <= 30 else xValue % 30 
         self.y = yValue if yValue <= 30 else yValue % 30
+        self.son = []
+        
+    def pushNode(self, node : 'Node'):
+        self.son.append(node)
